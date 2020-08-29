4caa7f8755ff91251f708a42c2ec3da8f1c71fd6
Ardit <arditahmeti96@gmail.com>
2020-08-29 20:47:04 +0200

    Initialized a java spring boot application and docker in  order to run the application using docker-compose. Also explained how to build and run the application iniside docker in the readme file.

---
+++ src/main/java/org/springframework/userservice/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package org.springframework.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * User service app
+ *
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
