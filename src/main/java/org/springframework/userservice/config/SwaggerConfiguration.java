696a4ff774dbdaac0fe8fe6899a394ef05dd2570
Ardit <arditahmeti96@gmail.com>
2020-08-30 18:29:28 +0200

    Added springfox swagger dependency in order to use swagger ui documentation. Configured the bootstrap in a new file SwaggerConfiguration and then documented the routes at user controller with their corresponding annotation.

---
+++ src/main/java/org/springframework/userservice/config/SwaggerConfiguration.java
@@ -0,0 +1,35 @@
+package org.springframework.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+import io.swagger.annotations.Api;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths((Predicate<String>) PathSelectors.any()).build().pathMapping("/")
+                .apiInfo(apiInfo()).useDefaultResponseMessages(false);
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("User Service API through Swagger UI").version("1.0").license("(C) Copyright Test")
+                .description("List of all the APIs of User Service App through Swagger UI");
+        return builder.build();
+    }
+}
--- src/main/java/org/springframework/userservice/controller/UserController.java
+++ src/main/java/org/springframework/userservice/controller/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.userservice.model.User;
 import org.springframework.userservice.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.net.URI;
 
 /**
@@ -13,6 +16,7 @@ import java.net.URI;
  */
 @RestController
 @RequestMapping("/users")
+@Api
 public class UserController {
     private UserService userService;
 
@@ -21,26 +25,31 @@ public class UserController {
     }
 
     @GetMapping
+    @ApiOperation("Get all users")
     public ResponseEntity getUsers(@RequestParam(required = false) String firstName) {
         return ResponseEntity.ok(userService.getAllUsers(firstName));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get user by id")
     public ResponseEntity getUserById(@PathVariable int id) {
         return ResponseEntity.ok(userService.getUserById(id).orElseThrow(() -> new RuntimeException("User doesn't exists.")));
     }
 
     @PostMapping
+    @ApiOperation("Store a user")
     public ResponseEntity createUser(@RequestBody User user) {
         return ResponseEntity.created(URI.create("")).body(userService.insertUser(user));
     }
 
     @PutMapping
+    @ApiOperation("Update a user")
     public ResponseEntity updateUser(@RequestBody User user) {
         return ResponseEntity.created(URI.create("")).body(userService.updateUser(user));
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Delete a user by id")
     public ResponseEntity deleteUser(@PathVariable int id) {
         return ResponseEntity.ok(userService.deleteUser(id));
     }
